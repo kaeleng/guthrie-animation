830b0076ac04dc2695b1cc775473aeae49c4788d
Kaelen Guthrie <kagu1593@colorado.edu>
2018-03-06 13:54:12 -0700

    sprite animtion

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+var animation = document.getElementById("animationdiv")
+
+function changeClassWhite(){
+	animation.className = "icon-animation";
+}
+
+function changeClassGreen(){
+	animation.className = "icon-animation2";
+}
+
+function changeClassYellow(){
+	animation.className = "icon-animation3";
+}
